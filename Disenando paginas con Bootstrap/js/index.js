1e124f1770bfca5726428a0d82ff4f8b5bb0053a
EdgarHdz17 <condoritos_170696@hotmail.com>
2022-03-10 19:02:30 -0600

    Final Proyect

--- Disenando paginas con Bootstrap/js/index.js
+++ Disenando paginas con Bootstrap/js/index.js
@@ -1,8 +1,24 @@
 
 $(function(){
-    $("[data-toggle='toltip']").toltip();
+    $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
     $('.carousel').carousel({
-        interval: 200
+        interval: 2000
+    });
+
+    $('#formContacto').on('show.bs.modal', function(e){
+        console.log("El modal de contacto se esta mostrando");
+    });
+
+    $('#formContacto').on('shown.bs.modal', function(e){
+        console.log("El modal de contacto se mostro");
+    });
+
+    $('#formContacto').on('hide.bs.modal', function(e){
+        console.log("El modal de contacto se oculta");
+    });
+
+    $('#formContacto').on('hidden.bs.modal', function(e){
+        console.log("El modal de contacto se oculto");
     });
 });
